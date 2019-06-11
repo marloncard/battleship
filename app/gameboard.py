82c055f0987532d88fe50c8f3091af9094a997e6
Marlon Card <marloncard@gmail.com>
2019-06-11 19:57:59 -0400

    Refactor, debug

--- app/gameboard.py
+++ app/gameboard.py
@@ -3,6 +3,8 @@ Ship
 Player
 Board
 """
+import sys
+import os
 
 BLANK = ' '
 SHIP  = 'S'
@@ -11,43 +13,51 @@ HIT   = 'X'
 
 
 class GameBoard:
+
     def __init__(self, width=5, height=5):
         self.width = width
         self.height = height
         self.ship_row = 1
         self.ship_col = 1
         
-        grid = []
+        self.grid = []
+
         for row_index in range(self.height):
             new_row = []
             for col_index in range(self.width):
                 new_row.append(BLANK)
-            grid.append(new_row)
-        self.grid = grid
-        self.ship_index = grid[ship_col][ship_row]
-        return self.grid
+            self.grid.append(new_row)
+        #self.grid = grid
+        self.ship_index = self.grid[self.ship_col][self.ship_row]
+        
+
+    def user_input(self):
+        print(self.ship_index)
+        while True:
+            for row in self.grid:
+                print(row)
+            coordinates = input("Enter coordinates: ")
+            c_col = int(coordinates[0]) - 1
+            c_row = int(coordinates[1]) - 1
+
+            print(c_col, c_row)
+            if c_col == self.ship_col and c_row == self.ship_row:
+                print("You won.")
+                self.grid[self.ship_row][self.ship_col]="X"
+                for row in self.grid:
+                    print(row)
+                sys.exit()
+            else:
+                self.grid[c_col][c_row]="M"
+                os.system('clear')
 
 
-    def user_input():
-        coordinates = input("Enter coordinates: ")
-        c_col = coordinates[0]
-        c_row = coordinates[1]
-        user_index = grid[c_col][c_row]
-        if user_index = ship_index:
-            print("You won.")
-        else:
-            user_index = MISS
-        if c_col == self.ship_col and c_row == self.ship_row:
-            index_
-        else:
-            pass
-        
-        
+                
+            
     
 
 
 if __name__ == '__main__':
     g = GameBoard()
-    print(g)
-    for row in g.board():
-        print(row)
+    g.user_input()
+    
