bce759424167adfd959d4b955447a257da93237d
Marlon Card <marloncard@gmail.com>
2019-06-11 17:13:12 -0400

    Initial changes

--- app/gameboard.py
+++ app/gameboard.py
@@ -1,3 +1,9 @@
+"""
+Ship
+Player
+Board
+"""
+
 BLANK = ' '
 SHIP  = 'S'
 MISS  = 'M'
@@ -6,10 +12,42 @@ HIT   = 'X'
 
 class GameBoard:
     def __init__(self, width=5, height=5):
+        self.width = width
+        self.height = height
+        self.ship_row = 1
+        self.ship_col = 1
+        
         grid = []
-        for row_index in range(height):
+        for row_index in range(self.height):
             new_row = []
-            for col_index in range(width):
+            for col_index in range(self.width):
                 new_row.append(BLANK)
             grid.append(new_row)
         self.grid = grid
+        self.ship_index = grid[ship_col][ship_row]
+        return self.grid
+
+
+    def user_input():
+        coordinates = input("Enter coordinates: ")
+        c_col = coordinates[0]
+        c_row = coordinates[1]
+        user_index = grid[c_col][c_row]
+        if user_index = ship_index:
+            print("You won.")
+        else:
+            user_index = MISS
+        if c_col == self.ship_col and c_row == self.ship_row:
+            index_
+        else:
+            pass
+        
+        
+    
+
+
+if __name__ == '__main__':
+    g = GameBoard()
+    print(g)
+    for row in g.board():
+        print(row)
